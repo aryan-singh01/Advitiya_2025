f09814aa655e988c8f6d969c66a931f459d3e54a
Chaitanya-Kalme <chaitanyakalme2005@gmail.com>
2026-01-02 22:25:17 +0530

    merchandise updated

--- src/app/(main)/merchandise/page.tsx
+++ src/app/(main)/merchandise/page.tsx
@@ -42,22 +42,71 @@ export const CloseIcon = () => {
 
 const merchandiseList = [
   {
-    title: "Advitiya T-Shirt",
-    src: "/advitiya t-shirt.jpg",
+    title: "Advitiya T-Shirt White",
+    src: "/merchandise/Advitiya_TShirt1.png",
     description: "",
     ctaText: "Order Now",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://docs.google.com/forms/d/e/1FAIpQLSftGIgeis0G31Ck6hGGIPwv49ppwOPrEGyECwrjXm43_UvHwg/viewform?usp=sharing&ouid=102438842226303481541",
     content: () => {
-      return <p>Advitiya T-Shirt Description</p>;
+      return <Image
+        src="/merchandise/T_Shirt_Size.png"
+        alt="Advitiya T-Shirt Size"
+        width={800}
+        height={1600}
+        sizes="(max-width: 768px) 100vw, 500px"
+        className="w-full h-auto object-contain rounded-lg"
+      />
     },
   },
   {
-    title: "Advitiya Hoodie",
-    src: "/advitiya hoodie.jpg",
+    title: "Advitiya T-Shirt Black",
+    src: "/merchandise/Advitiya_TShirt2.png",
     ctaText: "Order Now",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://docs.google.com/forms/d/e/1FAIpQLSftGIgeis0G31Ck6hGGIPwv49ppwOPrEGyECwrjXm43_UvHwg/viewform?usp=sharing&ouid=102438842226303481541",
     content: () => {
-      return <p>Advitiya Hoodie Description</p>;
+      return <Image
+        src="/merchandise/T_Shirt_Size.png"
+        alt="Advitiya T-Shirt Size"
+        width={800}
+        height={1600}
+        sizes="(max-width: 768px) 100vw, 500px"
+        className="w-full h-auto object-contain rounded-lg"
+      />
+    },
+  },
+  {
+    title: "Advitiya Hoodie Black",
+    src: "/merchandise/Advitiya_Hoodie2.png",
+    ctaText: "Order Now",
+    ctaLink: "https://docs.google.com/forms/d/e/1FAIpQLSd14zNmvLeM2eP5RgjA819bzABeeD_6ul1xljBtTZwSko-zXA/viewform?usp=header",
+    content: () => {
+      return <Image
+        src="/merchandise/Hoodie_Size.png"
+        alt="Advitiya Hoodie Size"
+        width={800}
+        height={1600}
+        sizes="(max-width: 768px) 100vw, 500px"
+        className="w-full h-auto object-contain rounded-lg"
+      />
+    },
+  },
+  {
+    title: "Advitiya Hoodie White",
+    src: "/merchandise/Advitiya_Hoodie1.png",
+    ctaText: "Order Now",
+    ctaLink: "https://docs.google.com/forms/d/e/1FAIpQLSd14zNmvLeM2eP5RgjA819bzABeeD_6ul1xljBtTZwSko-zXA/viewform?usp=header",
+    content: () => {
+      return (
+        <Image
+          src="/merchandise/Hoodie_Size.png"
+          alt="Advitiya Hoodie Size"
+          width={800}
+          height={1600}
+          sizes="(max-width: 768px) 100vw, 500px"
+          className="w-full h-auto object-contain rounded-lg"
+        />
+
+      )
     },
   },
 ];
@@ -89,21 +138,6 @@ function MerchandisePage() {
   useOutsideClick(ref, () => setActive(null));
 
 
-  return (
-    <>
-      <main className="relative min-h-screen">
-        {/* <StarsBackground/> */}
-        <div className="fixed inset-0 z-0 bg-gradient-to-br from-black via-gray-900 to-blue-950">
-          <StarsBackground className="w-full h-full" showShootingStars={true} />
-        </div>
-        <h1 className="relative z-10 mt-34 text-center justify-center font-bold text-5xl mb-5 text-white">
-          Coming Soon...
-        </h1>
-
-      </main>
-    </>
-  );
-
   // return (
   //   <>
   //     <main className="relative min-h-screen">
@@ -112,142 +146,158 @@ function MerchandisePage() {
   //         <StarsBackground className="w-full h-full" showShootingStars={true} />
   //       </div>
   //       <h1 className="relative z-10 mt-34 text-center justify-center font-bold text-5xl mb-5 text-white">
-  //         Buy Our Merchandise
+  //         Coming Soon...
   //       </h1>
-  //       <AnimatePresence>
-  //         {active && typeof active === "object" && (
-  //           <motion.div
-  //             initial={{ opacity: 0 }}
-  //             animate={{ opacity: 1 }}
-  //             exit={{ opacity: 0 }}
-  //             className="fixed inset-0 bg-black/20 h-full w-full z-10"
-  //           />
-  //         )}
-  //       </AnimatePresence>
-  //       <AnimatePresence>
-  //         {active && typeof active === "object" ? (
-  //           <div className="fixed inset-0  grid place-items-center z-[100]">
-  //             <motion.button
-  //               key={`button-${active.title}-${id}`}
-  //               layout
-  //               initial={{
-  //                 opacity: 0,
-  //               }}
-  //               animate={{
-  //                 opacity: 1,
-  //               }}
-  //               exit={{
-  //                 opacity: 0,
-  //                 transition: {
-  //                   duration: 0.05,
-  //                 },
-  //               }}
-  //               className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
-  //               onClick={() => setActive(null)}
-  //             >
-  //               <CloseIcon />
-  //             </motion.button>
-  //             <motion.div
-  //               layoutId={`card-${active.title}-${id}`}
-  //               ref={ref}
-  //               className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
-  //             >
-  //               <motion.div layoutId={`image-${active.title}-${id}`}>
-  //                 <img
-  //                   width={200}
-  //                   height={200}
-  //                   src={active.src}
-  //                   alt={active.title}
-  //                   className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
-  //                 />
-  //               </motion.div>
 
-  //               <div>
-  //                 <div className="flex justify-between items-start p-4">
-  //                   <div className="">
-  //                     <motion.h3
-  //                       layoutId={`title-${active.title}-${id}`}
-  //                       className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
-  //                     >
-  //                       {active.title}
-  //                     </motion.h3>
-  //                     <motion.p
-  //                       layoutId={`description-${active.description}-${id}`}
-  //                       className="text-neutral-600 dark:text-neutral-400 text-base"
-  //                     >
-  //                       {active.description}
-  //                     </motion.p>
-  //                   </div>
-
-  //                   <motion.a
-  //                     layout
-  //                     initial={{ opacity: 0 }}
-  //                     animate={{ opacity: 1 }}
-  //                     exit={{ opacity: 0 }}
-  //                     href={active.ctaLink}
-  //                     target="_blank"
-  //                     className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
-  //                   >
-  //                     {active.ctaText}
-  //                   </motion.a>
-  //                 </div>
-  //                 <div className="pt-4 relative px-4">
-  //                   <motion.div
-  //                     layout
-  //                     initial={{ opacity: 0 }}
-  //                     animate={{ opacity: 1 }}
-  //                     exit={{ opacity: 0 }}
-  //                     className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
-  //                   >
-  //                     {typeof active.content === "function"
-  //                       ? active.content()
-  //                       : active.content}
-  //                   </motion.div>
-  //                 </div>
-  //               </div>
-  //             </motion.div>
-  //           </div>
-  //         ) : null}
-  //       </AnimatePresence>
-  //       <ul className="relative z-10 max-w-2xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
-  //         {merchandiseList.map((card, index) => (
-  //           <motion.div
-  //             layoutId={`card-${card.title}-${id}`}
-  //             key={card.title}
-  //             onClick={() => setActive(card)}
-  //             className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
-  //           >
-  //             <div className="flex gap-4 flex-col  w-full">
-  //               <motion.div layoutId={`image-${card.title}-${id}`}>
-  //                 <Image
-  //                   width={100}
-  //                   height={100}
-  //                   src={card.src}
-  //                   alt={card.title}
-  //                   className="h-60 w-full  rounded-lg object-cover object-top"
-  //                 />
-  //               </motion.div>
-  //               <div className="flex justify-center items-center flex-col">
-  //                 <motion.h3
-  //                   layoutId={`title-${card.title}-${id}`}
-  //                   className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
-  //                 >
-  //                   {card.title}
-  //                 </motion.h3>
-  //                 <motion.p
-  //                   layoutId={`description-${card.description}-${id}`}
-  //                   className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
-  //                 >
-  //                   {card.description}
-  //                 </motion.p>
-  //               </div>
-  //             </div>
-  //           </motion.div>
-  //         ))}
-  //       </ul>
   //     </main>
   //   </>
   // );
+
+ return (
+    <>
+      <main className="relative min-h-screen">
+        {/* <StarsBackground/> */}
+        <div className="fixed inset-0 z-0 bg-gradient-to-br from-black via-gray-900 to-blue-950">
+          <StarsBackground className="w-full h-full" showShootingStars={true} />
+        </div>
+        <h1 className="relative z-10 mt-34 text-center justify-center font-bold text-5xl mb-5 text-white">
+          Buy Our Merchandise
+        </h1>
+        <AnimatePresence>
+          {active && typeof active === "object" && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed inset-0 bg-black/20 h-full w-full z-10"
+            />
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {active && typeof active === "object" ? (
+            <div className="fixed inset-0  grid place-items-center z-[100]">
+              <motion.button
+                key={`button-${active.title}-${id}`}
+                layout
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 0.05,
+                  },
+                }}
+                className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+                onClick={() => setActive(null)}
+              >
+                <CloseIcon />
+              </motion.button>
+              <motion.div
+                layoutId={`card-${active.title}-${id}`}
+                ref={ref}
+                className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              >
+                <motion.div layoutId={`image-${active.title}-${id}`}>
+                  <img
+                    width={200}
+                    height={200}
+                    src={active.src}
+                    alt={active.title}
+                    className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                  />
+                </motion.div>
+
+                <div>
+                  <div className="flex justify-between items-start p-4">
+                    <div className="">
+                      <motion.h3
+                        layoutId={`title-${active.title}-${id}`}
+                        className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+                      >
+                        {active.title}
+                      </motion.h3>
+                      <motion.p
+                        layoutId={`description-${active.description}-${id}`}
+                        className="text-neutral-600 dark:text-neutral-400 text-base"
+                      >
+                        {active.description}
+                      </motion.p>
+                    </div>
+
+                    <motion.a
+                      layout
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      href={active.ctaLink}
+                      target="_blank"
+                      className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
+                    >
+                      {active.ctaText}
+                    </motion.a>
+                  </div>
+                  <div className="pt-4 relative px-4">
+                    <motion.div
+                      layout
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                    >
+                      {typeof active.content === "function"
+                        ? active.content()
+                        : active.content}
+                    </motion.div>
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          ) : null}
+        </AnimatePresence>
+        <ul className="relative z-10 max-w-2xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
+          {merchandiseList.map((card, index) => (
+            <motion.div
+              layoutId={`card-${card.title}-${id}`}
+              key={card.title}
+              onClick={() => setActive(card)}
+              className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+            >
+              <div className="flex gap-4 flex-col  w-full">
+                <motion.div layoutId={`image-${card.title}-${id}`}>
+                  <Image
+                    width={100}
+                    height={100}
+                    src={card.src}
+                    alt={card.title}
+                    className="h-60 w-full  rounded-lg object-cover object-top"
+                  />
+                </motion.div>
+                <div className="flex justify-center items-center flex-col">
+                  <motion.h3
+                    layoutId={`title-${card.title}-${id}`}
+                    className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
+                  >
+                    {card.title}
+                  </motion.h3>
+                  <motion.p
+                    layoutId={`description-${card.description}-${id}`}
+                    className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
+                  >
+                    {card.description}
+                  </motion.p>
+                </div>
+              </div>
+            </motion.div>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+
 }
 
 export default MerchandisePage;
